e6f2c98171b95df04b5a86d2b74b24b3025ed75f
Leon Zhang <hileon@gmail.com>
2012-02-24 15:22:09 +0800

    add a test router branch

--- index.php
+++ index.php
@@ -12,72 +12,15 @@
 </head>
 <body>
 
+
 <div id="container" >
-  <div id="todoapp-logo" >  
-      <a id="to-todoapp" href="#app"></a>  
-      <h1>Todos</h1>    
-  </div>
-
-  <div id="todoapp" class="app-hide">
-    <a id="to-todoapp-logo" href="#logo"></a>
-    <div class="title">
-      <h1>Todos</h1>
-    </div>
-
-    <div class="content">
-
-      <div id="create-todo">
-        <input id="new-todo" placeholder="What needs to be done?" type="text" />
-        <span class="ui-tooltip-top" style="display:none;">Press Enter to save this task</span>
-      </div>
-
-      <div id="todos">
-        <ul id="todo-list"></ul>
-      </div>
-
-      <div id="todo-stats"></div>
-
-    </div>
-
-  </div>
+  <a href="#">home</a><br>
+  <a href="#app">app</a><br>
+  <a href="#app/:item_1">item 1</a><br>
+  <div id="loginfo" />
 </div>
 
 
-
-
-
-
-
-<!-- Templates 模板放在app js前面是因为,这些js不会被浏览器执行, 应该不会阻塞太多的时间. 而且app js里可能有多个script要执行, 这时候会阻塞浏览器的. -->
-  <script type="text/template" id="item-template">
-    <div class="todo <%= done ? 'done' : '' %>">
-      <div class="display">
-        <input class="check" type="checkbox" <%= done ? 'checked="checked"' : '' %> />
-        <div class="todo-text"></div>
-        <span class="todo-destroy"></span>
-      </div>
-      <div class="edit">
-        <input class="todo-input" type="text" value="" />
-      </div>
-    </div>
-  </script>
-
-  <script type="text/template" id="stats-template">
-    <% if (total) { %>
-      <span class="todo-count">
-        <span class="number"><%= remaining %></span>
-        <span class="word"><%= remaining == 1 ? 'item' : 'items' %></span> left.
-      </span>
-    <% } %>
-    <% if (done) { %>
-      <span class="todo-clear">
-        <a href="#">
-          Clear <span class="number-done"><%= done %></span>
-          completed <span class="word-done"><%= done == 1 ? 'item' : 'items' %></span>
-        </a>
-      </span>
-    <% } %>
-  </script>
 
 
 
